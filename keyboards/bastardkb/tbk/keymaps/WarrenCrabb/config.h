7e2f045ecf2d3b98b447b0d5154a464f67e00d41
Warren Crabb <warren.crabb@gmail.com>
2021-04-10 13:46:43 -0400

    base layout

--- keyboards/bastardkb/tbk/keymaps/WarrenCrabb/config.h
+++ keyboards/bastardkb/tbk/keymaps/WarrenCrabb/config.h
@@ -3,6 +3,6 @@
 #pragma once
 
 #define NO_AUTO_SHIFT_ALPHA
-#define NO_AUTO_SHIFT_SPECIAL
 #define TAPPING_TOGGLE 2
 #define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
--- keyboards/bastardkb/tbk/keymaps/WarrenCrabb/keymap.c
+++ keyboards/bastardkb/tbk/keymaps/WarrenCrabb/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 
     [BASE] = LAYOUT_split_4x6_5(
-    KC_EQL,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
+    KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
 //------------------------------------------------------------------------------------------------------------------//
     KC_TAB,    KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
 //-----------------------------------------------------------------------------------------------------//
@@ -81,26 +81,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LSPO,LCTL_T(KC_Z), LALT_T(KC_X),  KC_C,    KC_V,       KC_B,    KC_N,    KC_M, KC_COMM,  RALT_T(KC_DOT), RCTL_T(KC_SLSH),  KC_RSPC,
 //------------------------------------------------------------------------------------------------------------------//
                           TD(NPAD_LAYR), KC_SPC, KC_BSPC,     KC_DEL,  KC_ENT,  MO(MDIA),
-                                        XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX
+                                        XXXXXXX, KC_LGUI,     KC_RGUI, XXXXXXX
 
   ),
 
   [MODS] = LAYOUT_split_4x6_5(
-    KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,      KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
+     KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,      KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
 //-------------------------------------------------------------------------------------------------------------------//
-    RESET,   XXXXXXX, S(KC_F2), KC_LCBR, KC_RCBR, KC_VOLU,      C(KC_DOT),  KC_7,   KC_8,   KC_9, KC_PMNS, XXXXXXX,
+     RESET, XXXXXXX, S(KC_F2), KC_LCBR, KC_RCBR, KC_VOLU,       XXXXXXX,    KC_7,   KC_8,   KC_9, KC_PMNS, XXXXXXX,
 //-------------------------------------------------------------------------------------------------------------------//
-    XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, KC_VOLD,       KC_0,       KC_4,   KC_5,   KC_6, KC_PPLS, XXXXXXX,
+    XXXXXXX, XXXXXXX, KC_GRV, KC_LBRC, KC_RBRC, KC_VOLD,       KC_0,       KC_4,   KC_5,   KC_6, KC_PPLS, XXXXXXX,
 //-------------------------------------------------------------------------------------------------------------------//
-    XXXXXXX, KC_GRV, XXXXXXX, KC_MPRV, KC_MNXT, KC_MPLY,        KC_PDOT,    KC_1,   KC_2,   KC_3, KC_PENT, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, KC_MPRV, KC_MNXT, KC_MPLY,       KC_PDOT,    KC_1,   KC_2,   KC_3, KC_PENT, XXXXXXX,
 //-------------------------------------------------------------------------------------------------------------------//
                                _______, _______, _______,       _______, _______, _______,
-                                        _______, _______,       _______, _______
+                                        RESET, _______,       _______, _______
   ),
 
 
   [MDIA] = LAYOUT_split_4x6_5(
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,			KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,
 //-------------------------------------------------------------------------------------------------------------------//
     XXXXXXX, XXXXXXX, KC_HOME,   KC_UP,  KC_END, KC_VOLU,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 //-------------------------------------------------------------------------------------------------------------------//
